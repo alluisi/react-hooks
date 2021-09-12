6d8b80d2ccc6ae75ee94638c027100595fed3dfb
Alessandra Lamas Luisi <lecaluisi@hotmail.com>
2021-09-12 02:41:56 -0300

    Introdução - Primeiros componentes

--- naruto/src/pages/app/App.js
+++ naruto/src/pages/app/App.js
@@ -1,11 +1,27 @@
+import styled from 'styled-components';
 import narutoImg from '../../images/naruto.png';
 
 export function App() {
     return (
         <div className="App">
-            <p>quote xpto - Speaker</p>
-            <button>Quote No Jutsu</button>
-            <img src={narutoImg} alt='Naruto with a kunai' />
+            <Content>
+                <p>quote xpto - Speaker</p>
+                <button>Quote No Jutsu</button>
+                <NarutoImg src={narutoImg} alt='Naruto with a kunai' />
+            </Content>
         </div>
     );
 }
+
+const Content = styled.div`
+    height: 100vh;
+    padding: 0 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const NarutoImg = styled.img`
+    max-width: 50vh;
+    align-self: flex-end;
+`;
